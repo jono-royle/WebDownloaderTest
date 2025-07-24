08552160fc0d746e55a778ed87bd636fb9e5c96b
Jonathan Royle <jroyle@neurofenix.com>
2025-07-24 18:36:54 +0200

    Add retry connection option

--- WebDownloaderApp/Program.cs
+++ WebDownloaderApp/Program.cs
@@ -13,6 +13,8 @@ class Program
 
         var urls = config.GetSection("Urls").Get<string[]>();
         var outputFolder = config["OutputFolder"];
+        var retryMaximumString = config["URLRetryAttempts"];
+        int retryMaximum = 0;
 
         if (urls == null || urls.Length < 1)
         {
@@ -24,6 +26,14 @@ class Program
             Console.WriteLine("No output folder found in appsettings file");
             return;
         }
+        if(!string.IsNullOrEmpty(retryMaximumString))
+        {
+            int.TryParse(retryMaximumString, out retryMaximum);
+            if (retryMaximum < 0)
+            {
+                retryMaximum = 0;
+            }
+        }
         Directory.CreateDirectory(outputFolder);
 
         using (HttpClient client = new HttpClient())
@@ -32,7 +42,7 @@ class Program
             int i = 0;
             foreach (var url in urls)
             {
-                tasks.Add(DownloadWebpage(client, url, outputFolder, i));
+                tasks.Add(DownloadWebpage(client, url, outputFolder, i, retryMaximum));
                 i++;
             }
 
@@ -42,27 +52,31 @@ class Program
         }
     }
 
-    private static async Task DownloadWebpage(HttpClient client, string url, string outputFolder, int urlNumber)
+    private static async Task DownloadWebpage(HttpClient client, string url, string outputFolder, int urlNumber, int retryMaximum)
     {
+        int retryAttempts = 0;
+        bool succeededConnection = false;
         if (Uri.TryCreate(url, UriKind.Absolute, out Uri? uri))
         {
             try
             {
-                var response = await client.GetAsync(url);
-                if (response.IsSuccessStatusCode)
+                while (!succeededConnection && retryAttempts <= retryMaximum)
                 {
-                    byte[] data = await response.Content.ReadAsByteArrayAsync();
-                    string fileName = uri.Host + $"_{urlNumber}";
-                    string filePath = Path.Combine(outputFolder, fileName);
-
-                    FileStream fileStream = File.Create(filePath);
-                    await fileStream.WriteAsync(data, 0, data.Length);
-                    fileStream.Close();
-                    Console.WriteLine($"Downloaded webpage {urlNumber}, url: {url} to {fileName}");
-                }
-                else
-                {
-                    Console.WriteLine($"Response code for webpage {urlNumber}, url: {url} - {response.StatusCode}");
+                    var response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        succeededConnection = true;
+                        await CreateOutputFileFromPage(url, outputFolder, urlNumber, uri, response);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Response code for webpage {urlNumber}, url: {url} - {response.StatusCode}");
+                        retryAttempts++;
+                        if (retryAttempts <= retryMaximum)
+                        {
+                            Console.WriteLine($"Retrying connection for webpage {urlNumber}");
+                        }
+                    }
                 }
             }
             catch (Exception ex) 
@@ -76,5 +90,17 @@ class Program
         }
 
 
+    }
+
+    private static async Task CreateOutputFileFromPage(string url, string outputFolder, int urlNumber, Uri uri, HttpResponseMessage response)
+    {
+        byte[] data = await response.Content.ReadAsByteArrayAsync();
+        string fileName = uri.Host + $"_{urlNumber}";
+        string filePath = Path.Combine(outputFolder, fileName);
+
+        FileStream fileStream = File.Create(filePath);
+        await fileStream.WriteAsync(data, 0, data.Length);
+        fileStream.Close();
+        Console.WriteLine($"Downloaded webpage {urlNumber}, url: {url} to {fileName}");
     }
 }
