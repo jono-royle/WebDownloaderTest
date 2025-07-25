f04e5f1962d0079d905031a1174af3f5294aeb45
Jonathan Royle <jroyle@neurofenix.com>
2025-07-25 13:18:14 +0200

    Add thread safe queue of downloads above max limit

--- WebDownloaderApp/Program.cs
+++ WebDownloaderApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using Microsoft.Extensions.Configuration;
 
 
@@ -14,7 +15,9 @@ class Program
         var urls = config.GetSection("Urls").Get<string[]>();
         var outputFolder = config["OutputFolder"];
         var retryMaximumString = config["URLRetryAttempts"];
+        var maxConcurrentDownloadsString = config["MaxConcurrentDownloads"];
         int retryMaximum = 0;
+        int maxConcurrent = 1;
 
         if (urls == null || urls.Length < 1)
         {
@@ -34,22 +37,54 @@ class Program
                 retryMaximum = 0;
             }
         }
+        if (!string.IsNullOrEmpty(maxConcurrentDownloadsString))
+        {
+            int.TryParse(maxConcurrentDownloadsString, out maxConcurrent);
+            if (maxConcurrent < 1)
+            {
+                maxConcurrent = 1;
+            }
+        }
         Directory.CreateDirectory(outputFolder);
 
         using (HttpClient client = new HttpClient())
         {
             var webpageDownloader = new WebpageDownloader(client, outputFolder, retryMaximum);
-            List<Task> tasks = [];
-            int i = 0;
-            foreach (var url in urls)
+            ConcurrentQueue<(string url, int index)> queue = new ConcurrentQueue<(string, int)>();
+            List<Task> activeTasks = new List<Task>();
+            object lockObj = new object();
+            int initialDownloadListLength = Math.Min(urls.Length, maxConcurrent);
+            for (int i = 0; i < urls.Length; i++) 
             {
-                tasks.Add(webpageDownloader.DownloadWebpage(url, i));
-                i++;
+                if (i < initialDownloadListLength) 
+                {
+                    var task = StartDownload(webpageDownloader, urls[i], i, queue, activeTasks, lockObj);
+                    lock (lockObj) activeTasks.Add(task);
+                }
+                else
+                {
+                    queue.Enqueue((urls[i], i));
+                }
             }
 
-            await Task.WhenAll(tasks);
+            await Task.WhenAll(activeTasks);
 
             Console.WriteLine("All downloads completed");
+        }
+
+        static async Task StartDownload(WebpageDownloader downloader, string url, int index, ConcurrentQueue<(string url, int index)> queue, List<Task> activeTasks, object lockObj)
+        {
+            await downloader.DownloadWebpage(url, index);
+            if (queue.TryDequeue(out var next))
+            {
+                Console.WriteLine($"Dequeued next url download: {next.url}");
+                var nextTask = StartDownload(downloader, next.url, next.index, queue, activeTasks, lockObj);
+                lock (lockObj) activeTasks.Add(nextTask);
+            }
+            lock (lockObj)
+            {
+                activeTasks.RemoveAll(t => t.IsCompleted);
+            }
         }
     }
 }
